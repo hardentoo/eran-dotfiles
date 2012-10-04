44ea57319f83245e905cd634fb63a4827f89fb81
eran <eran@inictat.com>
2012-10-04 18:54:21 +0200

    tmuxgo.sh with no params will launch/attach a defaul session

--- bin/tmuxgo.sh
+++ bin/tmuxgo.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-SESSION=$1
+# Start a named tmux session (or attach, if exist)
+# If no name is specified, use 0
 
+if [ -n "$1" ]; then
+    SESSION=$1
+else
+    SESSION=0
+fi
 
 tmux has-session -t $SESSION
 if [ $? -eq 0 ]; then
@@ -13,9 +19,9 @@ fi
 
 tmux new-session -d -n "runserver" -s $SESSION
 
-tmux new-window -t $SESSION:1 -n "run tests"
-tmux new-window -t $SESSION:2 -n "vim tests" 'vim'
-tmux new-window -t $SESSION:3 -n "vim code" 'vim'
-tmux new-window -t $SESSION:4 -n "view source"
+#tmux new-window -t $SESSION:1 -n "run tests"
+#tmux new-window -t $SESSION:2 -n "vim tests" 'vim'
+#tmux new-window -t $SESSION:3 -n "vim code" 'vim'
+#tmux new-window -t $SESSION:4 -n "view source"
 
 tmux attach -t $SESSION
