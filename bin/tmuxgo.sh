5417bf17c16f548638ee60d6208484ada8514262
eran <eran@inictat.com>
2012-10-05 11:39:32 +0200

    tmuxgo - force 256 colors

--- bin/tmuxgo.sh
+++ bin/tmuxgo.sh
@@ -17,11 +17,11 @@ if [ $? -eq 0 ]; then
 fi
 
 
-tmux new-session -d -n "runserver" -s $SESSION
+tmux -2 new-session -d -n "runserver" -s $SESSION
 
 #tmux new-window -t $SESSION:1 -n "run tests"
 #tmux new-window -t $SESSION:2 -n "vim tests" 'vim'
 #tmux new-window -t $SESSION:3 -n "vim code" 'vim'
 #tmux new-window -t $SESSION:4 -n "view source"
 
-tmux attach -t $SESSION
+tmux -2 attach -t $SESSION
