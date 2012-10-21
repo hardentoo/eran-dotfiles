7f09c9f1b779b0a1a3a3ea49c99aaa41277a00f2
eran <eran@incitat.com>
2012-10-21 16:31:51 +0200

    add install.sh.  add a default backup folder

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Remember to run init_system.sh to download, install and configure your system"
+echo ""
+echo "Installing files..."
+echo "Warning - this will override your local configuration. Enter to continue."
+read temp
+
+echo "Updating submodules..."
+git submodule init
+git submodule update
+
+echo "Installing..."
+BASEPATH=$PWD
+cd ~/
+ln -sf .bash_aliases -t ~/
+ln -sf $BASEPATH/.bash_exports
+ln -sf $BASEPATH/.bash_profile
+ln -sf $BASEPATH/.gitconfig
+ln -sf $BASEPATH/.gitignore
+ln -sf $BASEPATH/.gitmodules
+ln -sf $BASEPATH/.tmux.conf
+ln -sf $BASEPATH/.vimrc
+ln -sf $BASEPATH/.vim
+
+echo "Registering your git name/email"
+echo -n "[Git] Enter your name: "
+read name
+echo -n "[Git] Enter your email address: "
+read email
+git config --global user.name "$name"
+git config --global user.email "$email"
+
